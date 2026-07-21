665f67c791049f6b25592be8eb10bd2f236b2f7b
Zelalem Belay <zelalembelay932@gmail.com>
2026-07-21 10:52:52 +0300

    Phase-2 add week-8

---
+++ Phase 2/Week-8/Programming Basics/JS/script.js
@@ -0,0 +1,150 @@
+// alert("your JS file is connected!!");
+
+// Question 1:
+
+// A
+console.log(24 > 3);
+// B
+console.log(2 < "12");
+// C
+console.log(0 == 2);
+// D
+console.log(2.0 === 2);
+// E
+console.log(2.0 == "2");
+// F. 
+console.log(2 < "John");
+// G. 
+console.log(2 > "John");
+// H. 
+console.log("2" < "2");
+// I. 
+console.log("2" > "12");
+// J. 
+console.log(1 == 1 || 3 == 2 || 3 == 7); 
+// K. 
+console.log(1 == 1 && 2 == 2 && 3 == 7);
+// L. 
+console.log(1 == 1 || 2 == 2 && 3 == 7);
+// M. 
+console.log(1 == true && 0 > true || "31" > "9" || 10 > 5 || !("2" == "two" || 1 == "1"));
+
+
+
+console.log("\n ======== Question 2 ===============");
+
+// 1
+// == It compares only the value.
+// ===  Compares both type and value.
+
+console.log("\n ======== 1 =========");
+
+// A
+console.log('1' === 1);
+// B
+console.log(1 == 1);
+// C
+console.log(1 === 1);
+
+console.log("Answer D");
+
+
+
+// 2
+// let x = (1 == true);
+// console.log(x);
+// Answer B True
+
+
+// 3
+// let x = 10;
+// let y = (x > 5) && (x < 15);
+
+// y = (10 > 5) && (10 < 15)
+
+// true && true  =>Answer D true
+
+// 4
+// let x = 5;
+// x+= 3; 
+// x = x + 3;
+// x = 5 + 3 => 8 
+// Answer: B 8
+
+
+//5 
+x = 10;
+console.log(x);
+y = x++;
+// y = 10++;
+
+console.log(x);
+// x++ is post-increment.
+// y = 10  ????????????
+// Answer: A  10
+
+
+// let x = 5;
+// console.log(x++);
+// console.log(x);
+
+
+
+
+// 6
+// let x = 1;
+// let y = (x !== 2);
+
+// (1 !== 2 ) =>  true
+// console.log(1 !== 2);
+// Answer: D true
+
+// 7
+// (+) in front of the string  convert into the number
+// +"2" + 2;
+// console.log(+"2" + 2);
+// Answer: 4
+
+
+// 8
+// modulo operator  % 
+// 7 % 3;
+// console.log(7 % 3);
+ // Answer: 1
+
+// 9
+2 + true;
+console.log(2 + true);
+
+// Answer: 3
+
+
+
+
+console.log("\n ======== Question 3 =========");
+let a = 1;
+let b = 2;
+let c = a + b;
+console.log(c);
+
+
+
+console.log("\n ======== Question 4 =========");
+
+let firstName = "Zelalem";
+let lastName = "Belay";
+
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+
+
+
+
+
+
+// let x = 5;
+// console.log(x++);
+// console.log(x);
+

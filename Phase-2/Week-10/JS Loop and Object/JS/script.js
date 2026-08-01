ed6d303bbd14e95e73abfb472eacd78548971beb
Zelalem Belay <zelalembelay932@gmail.com>
2026-08-01 15:06:40 +0300

    prints the first 10 integers in for loop

--- Phase-2/Week-10/JS Loop and Object/JS/script.js
+++ Phase-2/Week-10/JS Loop and Object/JS/script.js
@@ -17,21 +17,41 @@
 // }
 // console.log(`Success! It look ${attempts} rolls to get a 6.`);
 
-console.log("\n ===========      =========== ");
+console.log("\n ===========   add up   =========== ");
 
-const addUp = (a) =>{
-    if(typeof a !== "number") {
+// const addUp = (a) =>{
+//     if(typeof a !== "number") {
+//         return "Please enter a number value";
+//     }else if( a < 0){
+//         return "Please enter only posetive number ";
+//     }else{
+//         let result = 0;
+//         for (i = 1; i <= a; i++){
+//             result = result + i;
+//         }
+//         return result;
+//     }
+// }
+// addUp(4);
+// console.log(addUp(40));
+
+
+
+console.log("\n ===========  Question 1  =========== ");
+
+// Write a function that prints the first 10 integers on the console starting from the number 1 using the JavaScript for loop.
+
+// for loop
+function first10 (num){
+    if(isNaN(num)){
         return "Please enter a number value";
-    }else if( a < 0){
-        return "Please enter only posetive number ";
-    }else{
-        let result = 0;
-        for (i = 1; i <= a; i++){
-            result = result + i;
-        }
-        return result;
     }
+    if (num < 0){
+        return "Please enter only posetive number ";
+    }
+        for (num = 1; num <= 10; num++){
+            console.log(num);
+        }
 }
-addUp(4);
-console.log(addUp(40));
+first10(1)
 

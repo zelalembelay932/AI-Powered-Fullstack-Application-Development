18880a643cb140c8f699678bf700c70a7f5db21f
Zelalem Belay <zelalembelay932@gmail.com>
2026-08-02 23:06:05 +0300

    question 8 end

--- Phase-2/Week-10/JS Loop and Object/JS/script.js
+++ Phase-2/Week-10/JS Loop and Object/JS/script.js
@@ -73,32 +73,32 @@ console.log("\n ===========  Question 2  =========== ");
 // Write a function that takes a single number as an argument and prints the next 5 numbers in the console. Note: each output should be displayed on a new line.
 
 //1, Understand the Problem
-        // Takes one number as input.
-        // Prints the next 5 numbers.
-        // Prints each number on a new line.
+// Takes one number as input.
+// Prints the next 5 numbers.
+// Prints each number on a new line.
 
 // 2, solve it on paper
-    // fiveNumber(7);
-        // 8 9 10 11 12
-    // fiveNumber(20);
-        //  21 22 23 24 25
-    // fiveNumber(-3);
-        // -2 -1 0 1 2
+// fiveNumber(7);
+// 8 9 10 11 12
+// fiveNumber(20);
+//  21 22 23 24 25
+// fiveNumber(-3);
+// -2 -1 0 1 2
 
 // 3, Write Pseudo code
-    // Step 1
-        // Create a function that accepts one parameter.
-    // Step 2
-        // Start loop : need 5 repetitions.
-    // Step 3
-        // Begin at 1
-        // we want the next number
-        // we are eenter 7
-     // print 7 + 1 = 8
-    // Step 4
-        // Repeat at 5
-    // Step 5
-         // Print
+// Step 1
+// Create a function that accepts one parameter.
+// Step 2
+// Start loop : need 5 repetitions.
+// Step 3
+// Begin at 1
+// we want the next number
+// we are eenter 7
+// print 7 + 1 = 8
+// Step 4
+// Repeat at 5
+// Step 5
+// Print
 
 // 4, Pseudo code to JavaScript
 
@@ -117,34 +117,31 @@ console.log("\n ===========  Question 2  =========== ");
 // // fiveNumber(-3);
 //         // -2 -1 0 1 2
 
-
-
 console.log("\n ===========  Question 3  =========== ");
 //  Write a function that takes a single number and prints the sum of the next 10 numbers after the given number
 
-    //Test case: If you give 7 to the function, output should be: 125 - because (8 + 9 + 10+ 11+ 12+ 13+ 14+ 15+ 16+ 17 = 125)
-
+//Test case: If you give 7 to the function, output should be: 125 - because (8 + 9 + 10+ 11+ 12+ 13+ 14+ 15+ 16+ 17 = 125)
 
 //1, Understand the Problem
-    // Takes one number as input.
-    // Finds the next 10 numbers after it.
-    // Adds all  numbers.
-    //  print total sum
+// Takes one number as input.
+// Finds the next 10 numbers after it.
+// Adds all  numbers.
+//  print total sum
 // 2, solve it on paper
-    // 7 = (8 + 9 + 10+ 11+ 12+ 13+ 14+ 15+ 16+ 17)
-    // print 125
+// 7 = (8 + 9 + 10+ 11+ 12+ 13+ 14+ 15+ 16+ 17)
+// print 125
 
 // 3, Write Pseudo code
-    // Step 1
-        //  Create a function that accepts one parameter
-    // Step 2
-        // Create a variable called sum
-    // Step 3
-        // Repeat 10 times
-    // Step 4
+// Step 1
+//  Create a function that accepts one parameter
+// Step 2
+// Create a variable called sum
+// Step 3
+// Repeat 10 times
+// Step 4
 
-    // Step 5
-        // print
+// Step 5
+// print
 
 // 4, Pseudo code to JavaScript
 
@@ -154,7 +151,7 @@ console.log("\n ===========  Question 3  =========== ");
 //     for (let i = 1; i <= 10; i++){
 //         // sum += startNumber + i;
 //         sum = sum + (number + i);
-        
+
 //     }
 //     return sum;
 // }
@@ -163,26 +160,24 @@ console.log("\n ===========  Question 3  =========== ");
 // console.log(sumNextTen(7));
 
 // First iteration
-    // number = 7, // i = 1, sum = 0
-    //sum = sum + number + i  
-    // sum = sum + 7 + 1 
-    // sum = 0 + 8;
-    // sum = 8
+// number = 7, // i = 1, sum = 0
+//sum = sum + number + i
+// sum = sum + 7 + 1
+// sum = 0 + 8;
+// sum = 8
 
 //  Second iteration
-    // number = 7, i = 2, sum = 8
-    //sum = sum + number + i
-    // sum = sum + 7 + 2
-    // sum = 8 + 9;
-    // sum = 17
+// number = 7, i = 2, sum = 8
+//sum = sum + number + i
+// sum = sum + 7 + 2
+// sum = 8 + 9;
+// sum = 17
 
 // Rhird iteration
-    // number 7, i = 3, sum = 17
-    // sum = sum + number + i
-    // sum = 17 + 7 + 3 
-    // sum = 27
-
-
+// number 7, i = 3, sum = 17
+// sum = sum + number + i
+// sum = 17 + 7 + 3
+// sum = 27
 
 // Arithmetic Formula
 
@@ -194,10 +189,9 @@ console.log("\n ===========  Question 3  =========== ");
 // 6 + 7 + 8 + 9 + 10 + 11 + 12 + 13 + 14 + 15
 
 // 7 = (8 + 9 + 10+ 11+ 12+ 13+ 14+ 15+ 16+ 17)
-    // print 125
+// print 125
 
 // 8 + 9 + 10 + 11 + 12 + 13 + 14 + 15 + 16 + 17
-
 
 // (7+1) = 8
 // (7+2) = 9
@@ -214,31 +208,27 @@ console.log("\n ===========  Question 3  =========== ");
 
 // 10 * sum10Number + 55
 
-
-
-
 console.log("\n ===========  Question 4  =========== ");
 //  Write a function that takes an array as an argument and prints every element of the array on the console.
-    //  Test case: given the array a = [1, "Hello", 8, 44], output should look like this:
-
+//  Test case: given the array a = [1, "Hello", 8, 44], output should look like this:
 
 //1, Understand the Problem
-    // create a function that takes an array as a parameter
-    // Prints every element in the array
-    // each element  on a new line.
+// create a function that takes an array as a parameter
+// Prints every element in the array
+// each element  on a new line.
 
 // 2, solve it on paper
-    //  let numbers = [1, "Hello", 8, 44];
+//  let numbers = [1, "Hello", 8, 44];
 
 // 3, Write Pseudo code
-    // crate function receive an array.
-    // Start from the first element.
-    // print the element
-    // move to next
-    // continue more element
+// crate function receive an array.
+// Start from the first element.
+// print the element
+// move to next
+// continue more element
 
 // 4, Pseudo code to JavaScript
-    // For loop
+// For loop
 // function printArray(array) {
 //     for (let i = 0; i < array.length; i++){
 //         console.log(array[i]);
@@ -253,10 +243,8 @@ console.log("\n ===========  Question 4  =========== ");
 // 3 < 4 ✔
 // 4 < 4 ✘
 
-
-
 // while
-    // Keep repeating this block of code as long as the condition is true.
+// Keep repeating this block of code as long as the condition is true.
 // function printArray(array) {
 //     let i = 0;
 
@@ -273,16 +261,12 @@ console.log("\n ===========  Question 4  =========== ");
 
 // Index: 0   1       2   3
 // Value: 1  Hello    8   44
-// while (i < 4) 
-    // 0 < 4; T
-    // 1 < 4; T
-    // 2 < 4; T
-    // 3 < 4; T
-    // 4 < 4; F loop stop
-
-
-
-
+// while (i < 4)
+// 0 < 4; T
+// 1 < 4; T
+// 2 < 4; T
+// 3 < 4; T
+// 4 < 4; F loop stop
 
 // forEach()
 // const array = [1, "Hello", 8, 44];
@@ -290,14 +274,12 @@ console.log("\n ===========  Question 4  =========== ");
 //     console.log(element);
 // });
 
-
 // for...of
 // const array = [1, "Hello", 8, 44];
 
 // for (const element of array) {
 //     console.log(element);
 // }
-
 
 console.log("\n ===========  Question 5 =========== ");
 
@@ -311,9 +293,6 @@ console.log("\n ===========  Question 5 =========== ");
 
 // 5, Test
 
-
-
-
 // Write a function that takes an array as an argument and prints the total number of elements found in the array
 
 // const totalNumber = function (array){
@@ -321,7 +300,6 @@ console.log("\n ===========  Question 5 =========== ");
 // }
 // totalNumber([1, "Hello", 8, 44])
 // totalNumber(["world", 13]);
-
 
 // for
 
@@ -335,8 +313,6 @@ console.log("\n ===========  Question 5 =========== ");
 // }
 // totalNumber([1, "Hello", 8, 44])
 // totalNumber(["world", 13]);
-
-
 
 // While
 
@@ -353,12 +329,9 @@ console.log("\n ===========  Question 5 =========== ");
 // totalNumber([1, "Hello", 8, 44])
 // totalNumber(["world", 13]);
 
-
-
 console.log("\n ===========  Question 6 =========== ");
 
-// Write a function that takes an array of numbers as a parameter and logs in the console the sum of all the numbers in the array. 
-
+// Write a function that takes an array of numbers as a parameter and logs in the console the sum of all the numbers in the array.
 
 //  for loop
 
@@ -371,9 +344,6 @@ console.log("\n ===========  Question 6 =========== ");
 //     console.log(sum);
 // }
 // sumArray([5, 6, 99, 8, 76, 4, 68, 44]); // 310
-
-
-
 
 // While
 
@@ -389,7 +359,6 @@ console.log("\n ===========  Question 6 =========== ");
 // }
 // sumArray([5, 6, 99, 8, 76, 4, 68, 44]); // 310
 
-
 // for...of
 
 // function sumArray(array){
@@ -402,13 +371,8 @@ console.log("\n ===========  Question 6 =========== ");
 // }
 // sumArray([5, 6, 99, 8, 76, 4, 68, 44]); // 310
 
-
-
-
-
 console.log("\n ===========  Question 7 =========== ");
 //  Write a function that takes an array of all numbers as a parameter, subtracts the total sum of all odd numbers of the array from the total sum of all even numbers and logs the result in the console.
-
 
 // const evenOdd = function (array){
 //     let evenSum = 0;
@@ -423,12 +387,9 @@ console.log("\n ===========  Question 7 =========== ");
 //         }
 //     }
 //     console.log(evenSum - oddSum);
-// } 
+// }
 
 // evenOdd([5, 6, 99, 8, 76, 4, 68, 44]);
-
-
-
 
 // function evenOdd(array) {
 //     let evenSum = 0;
@@ -440,7 +401,7 @@ console.log("\n ===========  Question 7 =========== ");
 //             evenSum += value;
 //         } else {
 //             oddSum += value;
-            
+
 //         }
 //     }
 //     console.log(evenSum - oddSum);
@@ -448,32 +409,116 @@ console.log("\n ===========  Question 7 =========== ");
 // evenOdd([5, 6, 99, 8, 76, 4, 68, 44]);
 
 // iteration 1
-    // value = 5
-    // 5 % 2 === 0  F
-    // oddSum = oddSum + value;
-    // oddSum = 0 + 5
-    // oddSum = 5
+// value = 5
+// 5 % 2 === 0  F
+// oddSum = oddSum + value;
+// oddSum = 0 + 5
+// oddSum = 5
 // iteration 2
-    // value = 6
-    // 6 % 2 === 0  T
-    // evenSum = evenSum + value;
-    // evenSum = 0 + 6
-    // evenSum = 6
+// value = 6
+// 6 % 2 === 0  T
+// evenSum = evenSum + value;
+// evenSum = 0 + 6
+// evenSum = 6
 // iteration 3
-    // value = 99
-    // 99 % 2 === 0  F
-    // oddSum = oddSum + value;
-    // oddSum = 5 + 99
-    // oddSum = 104
+// value = 99
+// 99 % 2 === 0  F
+// oddSum = oddSum + value;
+// oddSum = 5 + 99
+// oddSum = 104
 // iteration 4
-    // value = 8
-    // 8 % 2 === 0  T
-    // evenSum = evenSum + value;
-    // evenSum = 6 + 8
-    // evenSum = 14
+// value = 8
+// 8 % 2 === 0  T
+// evenSum = evenSum + value;
+// evenSum = 6 + 8
+// evenSum = 14
+
+console.log("\n ===========  Question 8 =========== ");
+// Write a function that takes an array as a parameter and logs in the console the elements that have even indexes only. Notice: this question is not asking you to log elements with even value, but elements that are located on even indexes)
+
+//1, Understand the Problem
+// print only the elements indexes are even.
+
+// 2, solve it on paper
+
+//Index =>  0   1   2   3   4   5   6   7
+
+//Value =>  5   6   99  8   76  4   68  44
+
+//index 0 is even? T display
+//index 1 is even? F skip
+//index 2 is even? T display
+
+// 3, Write Pseudo code
+// Step 1
+// Create a function that accepts one parameter.
+// Step 2
+// create for loop check index even or odd
+// Step 3
+// if indx is even
+// print value at that index
+// step 4
+// if index is odd
+// skip it
+// Step 5
+// end
+
+// 4, Pseudo code to JavaScript
+
+function evenIndex (array) {
+    for (let i = 0; i < array.length; i++){
+        if (i % 2 === 0){
+            console.log(array[i]);
+        }
+    }
+}
+evenIndex([5, 6, 99, 8, 76, 4, 68, 44]);
 
 
 
 
 
-console.log("\n ===========  Question 7 =========== ");
+
+// for ... in
+// function evenIndex (array) {
+//     for (const index in array){
+//         if(index % 2 === 0){
+//             console.log(array[index]);
+//         }
+//     }
+// }
+
+// evenIndex([5, 6, 99, 8, 76, 4, 68, 44]);
+
+// iteration 1
+// i = 0
+// 0 % 2 === 0  T
+// print console.log(array[0]);
+// final print 5
+
+// iteration 2
+// i = 1
+// 1 % 2  === 0;  F
+// not print
+
+// iteration 3
+// i = 2
+// 2 % 2 === 0;  T
+// print  console.log(array[2]);
+// final print 99
+
+// iteration 4
+// i = 3
+// 3 % 2 === 0; F
+// not print
+
+// indec  0  1
+// Value  5  6
+
+const items = ["apple", "banana", "cherry", "date", "elderberry"];
+for (let i = 0; i < items.length; i++) {
+  if (i % 2 === 0) {
+    // console.log(items[i]);
+    console.log(i);
+  }
+}

5de15985baa59d8ef45e30f774369f232ceaaef8
Zelalem Belay <zelalembelay932@gmail.com>
2026-08-02 17:37:27 +0300

    Question 3 answer add

--- Phase-2/Week-10/JS Loop and Object/JS/script.js
+++ Phase-2/Week-10/JS Loop and Object/JS/script.js
@@ -5,15 +5,13 @@
 
 // console.log("Rolling the sice unit we go a 6...");
 
-
 // while (dicRoll !== 6) {
 
 //     dicRoll = Math.floor(Math.random() *6) + 1;
 //     attempts++;
-    
 
 //     console.log(`Attemp ${attempts}: Rolled a ${dicRoll}`);
-    
+
 // }
 // console.log(`Success! It look ${attempts} rolls to get a 6.`);
 
@@ -32,10 +30,8 @@ console.log("\n ===========   add up   =========== ");
 //         return result;
 //     }
 // }
-// addUp(4);
-// console.log(addUp(40));
 
-
+// console.log(addUp(10));
 
 console.log("\n ===========  Question 1  =========== ");
 
@@ -67,16 +63,14 @@ console.log("\n ===========  Question 1  =========== ");
 //     }
 //     let i = 1;
 //     while (i = 10){
-        
+
 //     }
 // }
 // console.log(first10(1));
 
-
 console.log("\n ===========  Question 2  =========== ");
 
 // Write a function that takes a single number as an argument and prints the next 5 numbers in the console. Note: each output should be displayed on a new line.
-
 
 //1, Understand the Problem
         // Takes one number as input.
@@ -92,30 +86,222 @@ console.log("\n ===========  Question 2  =========== ");
         // -2 -1 0 1 2
 
 // 3, Write Pseudo code
-        // Step 1
-            // Create a function that accepts one parameter.
-        // Step 2
-            // Start loop : need 5 repetitions.
-        // Step 3
-            // Begin at 1 
-                // we want the next number
-                // we are eenter 7 
-                        // print 7 + 1 = 8
-        // Step 4
-            // Repeat at 5
-        // Step 5
-            // Print
+    // Step 1
+        // Create a function that accepts one parameter.
+    // Step 2
+        // Start loop : need 5 repetitions.
+    // Step 3
+        // Begin at 1
+        // we want the next number
+        // we are eenter 7
+     // print 7 + 1 = 8
+    // Step 4
+        // Repeat at 5
+    // Step 5
+         // Print
 
 // 4, Pseudo code to JavaScript
 
-const fiveNumber = (num) => {
-    for (let i = 1; i <= 5;  i++){
-        console.log(num + i);
-    }
-} 
+// 1. for Loop
+// const fiveNumber = (num) => {
+//     for (let i = 1; i <= 5;  i++){
+//         console.log(num + i);
+//     }
+// }
+// 5, Test
+
 // fiveNumber(7);
-        // 8 9 10 11 12
-// fiveNumber(20);
-        //  21 22 23 24 25
-fiveNumber(-3);
-        // -2 -1 0 1 2
+//         // 8 9 10 11 12
+// // fiveNumber(20);
+//         //  21 22 23 24 25
+// // fiveNumber(-3);
+//         // -2 -1 0 1 2
+
+
+
+console.log("\n ===========  Question 3  =========== ");
+//  Write a function that takes a single number and prints the sum of the next 10 numbers after the given number
+
+    //Test case: If you give 7 to the function, output should be: 125 - because (8 + 9 + 10+ 11+ 12+ 13+ 14+ 15+ 16+ 17 = 125)
+
+
+//1, Understand the Problem
+    // Takes one number as input.
+    // Finds the next 10 numbers after it.
+    // Adds all  numbers.
+    //  print total sum
+// 2, solve it on paper
+    // 7 = (8 + 9 + 10+ 11+ 12+ 13+ 14+ 15+ 16+ 17)
+    // print 125
+
+// 3, Write Pseudo code
+    // Step 1
+        //  Create a function that accepts one parameter
+    // Step 2
+        // Create a variable called sum
+    // Step 3
+        // Repeat 10 times
+    // Step 4
+
+    // Step 5
+        // print
+
+// 4, Pseudo code to JavaScript
+
+// function sumNextTen(number) {
+//     let sum = 0
+
+//     for (let i = 1; i <= 10; i++){
+//         // sum += startNumber + i;
+//         sum = sum + (number + i);
+        
+//     }
+//     return sum;
+// }
+
+// 5, Test
+// console.log(sumNextTen(7));
+
+// First iteration
+    // number = 7, // i = 1, sum = 0
+    //sum = sum + number + i  
+    // sum = sum + 7 + 1 
+    // sum = 0 + 8;
+    // sum = 8
+
+//  Second iteration
+    // number = 7, i = 2, sum = 8
+    //sum = sum + number + i
+    // sum = sum + 7 + 2
+    // sum = 8 + 9;
+    // sum = 17
+
+// Rhird iteration
+    // number 7, i = 3, sum = 17
+    // sum = sum + number + i
+    // sum = 17 + 7 + 3 
+    // sum = 27
+
+
+
+// Arithmetic Formula
+
+// const sum10Number = (num) => {
+//     return 10 * num + 55;
+// }
+// console.log(sum10Number(5));
+
+// 6 + 7 + 8 + 9 + 10 + 11 + 12 + 13 + 14 + 15
+
+// 7 = (8 + 9 + 10+ 11+ 12+ 13+ 14+ 15+ 16+ 17)
+    // print 125
+
+// 8 + 9 + 10 + 11 + 12 + 13 + 14 + 15 + 16 + 17
+
+
+// (7+1) = 8
+// (7+2) = 9
+// (7+3) = 10
+// (7+4) = 11
+// (7+5) = 12
+// (7+6) = 13
+// (7+7) = 14
+// (7+8) = 15
+// (7+9) = 16
+// (7+10) = 17
+
+// 1+2+3+4+5+6+7+8+9+10 = 55
+
+// 10 * sum10Number + 55
+
+
+
+
+console.log("\n ===========  Question  =========== ");
+
+
+//1, Understand the Problem
+
+
+// 2, solve it on paper
+
+
+// 3, Write Pseudo code
+
+// 4, Pseudo code to JavaScript
+
+
+// 5, Test
+
+
+
+
+
+
+
+
+console.log("\n ===========  Question  =========== ");
+
+
+//1, Understand the Problem
+
+// 2, solve it on paper
+
+
+// 3, Write Pseudo code
+
+// 4, Pseudo code to JavaScript
+
+
+// 5, Test
+
+
+
+
+
+
+
+
+
+
+
+console.log("\n ===========  Question  =========== ");
+
+
+//1, Understand the Problem
+
+// 2, solve it on paper
+
+
+// 3, Write Pseudo code
+
+// 4, Pseudo code to JavaScript
+
+
+// 5, Test
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("\n ===========  Question  =========== ");
+
+
+//1, Understand the Problem
+
+// 2, solve it on paper
+
+
+// 3, Write Pseudo code
+
+// 4, Pseudo code to JavaScript
+
+
+// 5, Test

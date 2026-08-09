2ec7ad913c416446a4cef4ee80a07431007a386c
Zelalem Belay <zelalembelay932@gmail.com>
2026-08-09 15:44:25 +0300

    irst array can be nested inside the second.

--- Phase-2/Week-11/Advanced JavaScript Algorithms/JS/script.js
+++ Phase-2/Week-11/Advanced JavaScript Algorithms/JS/script.js
@@ -13,8 +13,7 @@
 // Step 6 
     // Test
 
-
-
+console.log("\n ============ Question 1 ===========");
 // Q1,
     // What is Hiding Amongst the Crowd ?
 
@@ -54,7 +53,8 @@
             // for every letter
             //  How?
                // Use CharAt method to take out a single letter from the word check if the letter UPPER or lower
-                // charAt(word)
+        // only for loop  
+        // charAt(word)
             // Source - http://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charAt
     // Step 4
             // check if the letter is upper case or lower case 
@@ -109,21 +109,24 @@
 
 // Step 6 
     // Test
+// // For loop
+// const wordDetect = (a)  => {
+
+//     let hiddenWord = "";
+
+//     for (i = 0; i < a.length; i++){
+
+//         let word = a.charAt(i)
+//         if(word === word.toLowerCase()){
+//             hiddenWord = hiddenWord + word;
+//         }
+//     }
+//     // console.log(word);
+//     console.log(hiddenWord);
+// }
 
 
-
-const wordDetect = (a)  => {
-
-    for (i = 0; i < a.length; i++){
-        // how do i a singel letter out of a string in javaScript
-        let word = a.charAt(i)
-        console.log(word);
-    }
-    return wordDetect;
-}
-
-
-wordDetect("UcUNFYGaFYGtHHDHD");
+// wordDetect("UcUNFYGaFYGtHHDHD");
 // "cat"
 
 // function detectWord(crowd) {
@@ -135,3 +138,116 @@ wordDetect("UcUNFYGaFYGtHHDHD");
 
 // console.log(detectWord("bUUNFYGuFYrFDgHHlFDaDHrD"));
 // "burglar"
+
+
+
+
+
+
+
+
+console.log("\n ===========  Question 2 =========== ");
+
+
+// Create a functon that return true if the first array can be nested inside the second.
+
+    // arr1 can be nested inside arr2 if:
+        // arr1 min is greater than arr2's min
+        // arr1 max is less than arr2 max
+// example
+    // canNest([1, 2, 3, 4], [0, 6]) => true
+    // canNest([3, 1], [4, 0]) => true
+    // canNest([9, 9, 8], [8, 9]) => false
+    // canNest([1, 2, 3, 4], [2, 4]) => false
+
+
+
+
+// Step 1
+    // Understand the problem
+        // we have 2 arrays arr1 and arr2.
+        // We need to all arr1 fits strictly inside of  the arr2.
+            // arr1 can be nested inside arr2 IF:
+
+                // arr1 minimum > arr2 minimum
+
+                // arr1 maximum < arr2 maximum
+        // Both condition must be true
+
+// Step 2
+    // Solve it on paper
+    // arr2
+        //0 |--------------------- ----| 6
+        //        arr1
+        //       1 |--------------| 4
+
+        // canNest([1, 2, 3, 4], [0, 6])
+            // Mini
+                // arr1 = [1, 2, 3, 4]
+                //     min(arr1) = 1
+
+                //     arr2 = [0, 6]
+                //     min(arr2) = 0
+            //  1 > 0 => True
+            // max
+                // max(arr1) = 4
+                // max(arr2) = 6
+            // 4 < 6 => true
+
+            
+// Step 3
+    // Create a general Solution to applay step by step
+        // find arr1 mini
+        // find arr2 mini
+
+        // find arr2 max
+        // find arr2 max
+
+        // compare the minimum values
+        // compare the max values
+        // both must  be true
+    // return result
+// Step 4
+    // Write Pseudo code
+        // Step 1
+            // define function canNest that tekes argument Arr1 and arr2
+        // Step 2
+            // find the first array mini value
+            // find the second array mini value
+        // Step 3
+            // find the first array max value
+            // find the second array max value
+        // Step 4
+            // if arr1 mini > arr2 mini and arr1 max and arr2 max
+            // return true
+        // Step 4
+            // else return False
+// Step 5
+    // Translate to JavaScript Code
+
+const canNest = function (arr1, arr2){
+
+    return (
+        // arr1 minimum > arr2 minimum
+        Math.min(...arr1) > Math.min(...arr2) &&
+        // arr1 maximum < arr2 maximum
+        Math.max(...arr1) < Math.max(...arr2)
+    );
+
+    // const arr1 = [5, 3, 9, 1];
+    // // Without spread operator:
+    // Math.min(arr1); // Returns NaN 
+
+    // // With spread operator:
+    // Math.min(...arr1); // Returns 1
+
+
+}
+
+// Step 6 
+    // Test
+console.log(canNest([1, 2, 3, 4], [0, 6])); //=> true
+
+console.log( canNest([3, 1], [4, 0])); // => true
+console.log(canNest([9, 9, 8], [8, 9])); //=> false
+console.log(canNest([1, 2, 3, 4], [2, 4])); //=> false

4ecec3f6a9e96319f0e9510172f3f51bfb9aaab9
Zelalem Belay <zelalembelay932@gmail.com>
2026-08-26 14:01:14 +0300

    DOM login signup form JavaScript done

--- Phase-2/Week-12/form validation/JS/script.js
+++ Phase-2/Week-12/form validation/JS/script.js
@@ -1,8 +1,10 @@
 const form = document.getElementById('form');
+
 const firstname_input = document.getElementById("firstname-input");
+
 const email_input = document.getElementById("email-input");
 const password_input = document.getElementById("password-input");
-const repeatPassword_input = document.getElementById("repeat-password-input");
+const repeat_Password_input = document.getElementById("repeat-password-input");
 
 const error_message = document.getElementById("error-message");
 
@@ -10,15 +12,15 @@ const error_message = document.getElementById("error-message");
 
 form.addEventListener('submit', (e) =>{
     let errors = []
-    if (firstname) {
+    if (firstname_input) {
         // if we have a firstname input we are in the signup
-        errors = getSignupFormErrors(firstname_input.value, email_input.value, password_input.value, repeatPassword_input.value)
+        errors = getSignupFormErrors(firstname_input.value, email_input.value, password_input.value, repeat_Password_input.value)
     } else {
         // if we don't have a firstname input then we are in the login page
         errors = getSignupFormErrors(email_input.value, password_input.value);
     }
 
-    if (e.length > 0) {
+    if (errors.length > 0) {
         // if there are any errors
         e.preventDefault();
         error_message.innerText = errors.join(". ")
@@ -39,8 +41,26 @@ function getSignupFormErrors (firstname, email, password, repeatPassword) {
         errors.push('Password is required')
         password_input.parentElement.classList.add("incorrect");
     }
+    if(password.length <8){
+        errors.push('Password must have 8 characters')
+        password_input.parentElement.classList.add("incorrect");
+    }
+    if(password !== repeatPassword){
+        errors.push('Password dose not match repeated password')
+        password_input.parentElement.classList.add("incorrect");
+        repeat_Password_input.parentElement.classList.add("incorrect");
+    }
     return errors;
 }
 
 
+const allInputs = [firstname_input, email_input, password_input, repeat_Password_input];
 
+allInputs.forEach(input => {
+    input.addEventListener('input', () =>{
+        if(input.parentElement.classList.contains('incorrect')){
+            input.parentElement.classList.remove('incorrect')
+            error_message.innerText = ''
+        }
+    })
+})

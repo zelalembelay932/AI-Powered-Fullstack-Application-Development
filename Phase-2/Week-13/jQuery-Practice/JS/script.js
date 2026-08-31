72e5daad55de6c63c4467e3fca087de5d0bdd175
Zelalem Belay <zelalembelay932@gmail.com>
2026-08-31 15:38:08 +0300

    jQuery Note

--- Phase-2/Week-13/jQuery-Practice/JS/script.js
+++ Phase-2/Week-13/jQuery-Practice/JS/script.js
@@ -21,14 +21,14 @@
 // jQuery
 // const sample1 = $("#sample1");
 
-// //1.2  print element itself on the console
+// // //1.2  print element itself on the console
 // console.log(sample1);
 
-// // 1.3  print the content/text of the element
+// // // 1.3  print the content/text of the element
 // sample1.text();
 // console.log(sample1.text());
 
-// Modern jQuery shorthand
+// // Modern jQuery shorthand
 // $(function () {
 //     // 1.1. Select the element with an id of "sample1"
 //     const sample1 = $("#sample1");
@@ -57,10 +57,10 @@ console.log("\n=================== 2 =================");
 // const techCompanies = $("#techCompanies");
 // console.log(techCompanies);
 
-// 2.2. How many tech companies are listed under the ul element with an id of "techCompanies"?
+// // 2.2. How many tech companies are listed under the ul element with an id of "techCompanies"?
 
 
-// const  count =  $("#techCompanies li").length;
+// const count =  $("#techCompanies li").length;
 // console.log(count);
 
 
@@ -126,13 +126,13 @@ console.log("\n=================== 2 =================");
         //   console.log(this);
         // });
         // this refer to the curent HTML element
-// "this" is the cormal DOM element
+// "this" is the normal DOM element
         // but we are working with jQuery methods
 
         // Eg:-
-        //this.text(); // doesn't work because .text() is a jQuery method.
+        // this.text(); // doesn't work because .text() is a jQuery method.
         // we are convert current DOM to jQuery
-        //$(this).text() // this means get the text of the current <li>
+        // $(this).text() // this means get the text of the current <li>
 // .trim() => removes unnecessary spaces
 
 
@@ -224,20 +224,19 @@ console.log("\n=================== 3 =================");
 
 // $ => is shortcut used by jQuery
 // document => represent the current HTML page 
-// $(document) => geve the DYML document to jQuery
+// $(document) => geve the HTML document to jQuery
 // .ready() => wait until the HTML document has finished loading, then run this function
 
 
-// $(document).ready(function () {
+// $(document).ready(function () { 
 //     // $("#numberForm")
 //         // select form id with jQuery 
 //     // .on("submit", ...)
 //         // Event Listen for form submission
 //     $("#numberForm").on("submit", function (event) {
 
-
 //         // Prevent the browser from refreshing/submitting normally
-//         event.preventDefault();
+//    event.preventDefault();     
 
 //         // Get the values from the two form input fields
 //         const value1 = $("#number1").val().trim();
@@ -290,7 +289,7 @@ console.log("\n=================== 3 =================");
 
 //  Method 2 
 
-//When the HTML document is ready, run this function.
+// // When the HTML document is ready, run this function.
 // $(function () {
   
 //   $("#numberForm").on("submit", function (event) {
@@ -304,8 +303,8 @@ console.log("\n=================== 3 =================");
 //       return;
 //     }
 
-//     const number1 = Number(value1);
-//     const number2 = Number(value2);
+//     // const number1 = Number(value1);
+//     // const number2 = Number(value2);
 
 //     if (Number.isNaN(number1) || Number.isNaN(number2)) {
 //       showError();
@@ -333,34 +332,34 @@ console.log("\n=================== 3 =================");
 
 console.log("\n=================== 4 =================");
 
-// $(function () {
-//     // When this form is submitted, run this code.
-//     $("#userForm").on("submit", function (event) {
-//         event.preventDefault();
+$(function () {
+    // When this form is submitted, run this code.
+    $("#userForm").on("submit", function (event) {
+        event.preventDefault();
 
-//         const firstName = $("#firstName").val().trim();
-//         const lastName = $("#lastName").val().trim();
-//         const email = $("#email").val().trim();
+        const firstName = $("#firstName").val().trim();
+        const lastName = $("#lastName").val().trim();
+        const email = $("#email").val().trim();
 
-//         if (firstName === "" || lastName === "" || email === ""){
-//             $("#err").text("Please provide all required fields");
-//             return;
-//         }
-//         // Remove any error message
-//         $("#err").text("")
-//         // Hide the form
-//         $("#userForm").hide();
+        if (firstName === "" || lastName === "" || email === ""){
+            $("#err").text("Please provide all required fields");
+            return;
+        }
+        // Remove any error message
+        $("#err").text("")
+        // Hide the form
+        $("#userForm").hide();
 
-//         // display values enterd by the user
+        // display values enterd by the user
 
-//         $("#userOutput").html(` 
-//             <p> First Name: ${firstName} </p>
-//             <p> Last Name: ${lastName} </p>
-//             <p> Email : ${email} </p>
+        $("#userOutput").html(` 
+            <p> First Name: ${firstName} </p>
+            <p> Last Name: ${lastName} </p>
+            <p> Email : ${email} </p>
 
-//             `);
-//     })
-// })
+            `);
+    })
+})
 
 
 
--- Phase-2/Week-13/jQuery/Note/JS/jQuery.js
+++ Phase-2/Week-13/jQuery/Note/JS/jQuery.js
@@ -73,12 +73,101 @@ console.log("\n ===============");
         // jQuery uses feature detction to findthe best way t do things
     // 
 
-
-
-// What can you do with jQuery?
-
-
 // Adding jQuery to your web page 
+    // official website for jQuery
+        // https://jquery.com/
+    // how to add jQuery to your website
+        // 1, Download and attach it to your working project folder and HTML file
+        // 2, Include the CDN link in your HTML file
 
 
 // Selecting (Finding elements with jQuery)
+
+    // jQuery => $()
+        // the jQuery() returns a jQuery object of the select element 
+// Types of selectors    
+    // This is very similar with the CSS selectors
+        // #id
+        // .Class
+        // element
+
+    // Eg:- For ID
+        // using JavaScript
+            // const el = document.getElementById("your_id")
+            // console.log(el);;
+        // Using Jquery
+            // const el = $("#your_id");
+            // console.log(el); 
+    // Eg:- For Class
+        // using JavaScript
+            // const el = document.getElementById("your_class")
+            // console.log(el);;
+        // Using Jquery
+        // include . (dot) in your parameter
+            // const el = $(".your_class");
+            // console.log(el); 
+    // Eg:- For Element
+        // using JavaScript
+            // const el = document.getElementById("li")
+            // console.log(el);;
+        // Using Jquery
+            // const el = $("li");
+            // console.log(el); 
+
+
+
+// Selecting  element with jQuery(filters)
+
+// 1, first()
+// select first h1 element to HTML document 
+    // console.log($(h1));
+// Select first h1 HTML element then add it background red
+    // $("h1").first().css("background-color", "red");
+// Select first CSS h1 element then add it background red
+    // $("h1:first").css("background-color", "red");
+        // "h1:first" => this method is CSS not jQuery
+
+// 2, last()
+    // Select Last h1 HTML element then add it background red
+    // $("h1").last().css("background-color", "red");
+
+// 3, even()
+    // select even h1 element to instade of #companies id then add it background color blue
+    // $("#companies h1").even().css("background-color", "blue");
+
+// 4, odd()
+    // select odd h1 element to instade of #companies id then add it background color blue
+    // $("#companies h1").odd().css("background-color", "blue");
+// 5, nth-child()
+    // which is css selecter we are select id companies then select and affect h1 child 1 BG to blue
+    // $("#companies h1:nth-child(1)").css("background-color", "blue");
+// 6, has(selector)
+    // it is always expact HTML tags
+        // parent div hono westu button yalew div select senaderg
+
+        // Eg:-
+            // $("div").has("button").css("background-color", "black");
+
+// 7, :contains(text)
+    // $("h1:contains(A)").css("background-color", "red")
+
+// 8, empty();
+    // $("div.buttons").empty();
+
+    // VISIBILITY
+// 9, hide();
+    // $("h1[id=one]").hide();
+
+// 10, visible()
+    // $("h1:visible").css("background-color", "red");
+// 11, show()
+    // $("h1[id=one]").show()
+
+
+
+
+
+
+
+
+

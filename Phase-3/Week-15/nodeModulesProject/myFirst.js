8854b43eb361849b383b87378b140e9d27792e34
Zelalem Belay <zelalembelay932@gmail.com>
2026-09-13 15:28:34 +0300

    Week-15 note The IIFE way

---
+++ Phase-3/Week-15/nodeModulesProject/myFirst.js
@@ -0,0 +1,42 @@
+
+//Node Modules and Node Server - Practice Exercises
+
+// Questions 1
+        // Create a module called "myFirst":
+    
+    // a. Inside the "myFirst" module, log a simple text that says "My first module" on the console.
+
+    console.log("My first module");
+    // Print  in the terminal 
+    
+    //b. Run the module and see the output of the code you just wrote in this module
+
+
+    // c. Write a function called "myMultiplier" inside of your "myFirst" module. This function takes a single number as a parameter and returns a value that is 2 times the parameter
+
+        function myMultiplier (number){
+        return number * 2;
+        }
+
+    // d. Execute the function inside the module. Pass the number 4 to the function
+        const result = myMultiplier(4)
+
+    // e. Save the returned value in a variable and log the variable on the console. Now, run your module on the terminal to see the printed output
+
+        console.log(result);
+    // f. Export your "myMultiplier" function so that other modules can use it
+        module.exports = myMultiplier;
+
+
+//   // Node Executes it like this:-
+
+          //  myMultiplier(4)
+                
+                    // number = 4
+                        // 4 * 2
+                            // 8
+                                // return 8
+                    // result = 8
+
+
+

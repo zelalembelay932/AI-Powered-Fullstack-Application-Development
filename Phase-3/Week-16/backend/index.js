df0a47e82f89fb391aabfdf32d5bb79e0b6d8407
Zelalem Belay <zelalembelay932@gmail.com>
2026-09-20 16:45:12 +0300

    add in mysql database create customers, address, company tables created

--- Phase-3/Week-16/backend/index.js
+++ Phase-3/Week-16/backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 const mysql = require("mysql2");
 
 require('dotenv').config()
-console.log(process.env);
+// console.log(process.env);
 // Step 3
     // pass credential to createConnection
 const conn = mysql.createConnection({
@@ -30,6 +30,32 @@ conn.connect((err) =>{
 app.get("/", (req, res) =>{
     res.send("Hello Class")
 });
+
+// Route create - table 
+app.get("/create-table", (req, res) =>{
+    // putting Query on a variable
+    let customers = `CREATE TABLE if not exists customers(customer_id int auto_increment, name VARCHAR(255) not null, PRIMARY KEY (customer_id))`;
+
+    let address = `CREATE TABLE if not exists address(address_id int auto_increment, customer_id int(11) not null, address VARCHAR(255) not null, PRIMARY KEY (address_id), FOREIGN KEY (customer_id) REFERENCES customers (customer_id))`;
+
+    let company = `CREATE TABLE if not exists company( company_id int auto_increment, customer_id int not null, company VARCHAR(255) not null, PRIMARY KEY (company_id), FOREIGN KEY (customer_id) REFERENCES customers (customer_id)
+        )`;
+
+// exexute the query's we wrote above tables
+conn.query(customers, (err,) => {
+    if (err) console.log(`Error Found: ${err}`);
+})
+conn.query(address, (err,) => {
+    if (err) console.log(`Error Found: ${err}`);
+});
+conn.query(company, (err,) => {
+    if (err) console.log(`Error Found: ${err}`);
+
+    res.send("Table created successfully! ");
+})
+});
+
+
 
 app.listen(3000, () => console.log(" Server runing: http://localhost:3000"))
 
